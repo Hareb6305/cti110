cbc57b70f444ebf04f8f54a60abf3738e734dc04
Hareb6305 <hareb6305@student.faytechcc.edu>
2024-11-14 10:51:21 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_HareBajaya.py
@@ -0,0 +1,9 @@
+ # Bajaya Hare
+ # November 12, 2024
+ # P1 LAB1
+ # A program that says "Hello, (First Name Last Name)! Welcome to CTI 110
+
+firstname= input('Enter your First Name:')
+lastname= input ('Enter your Last Name:')
+
+print('Hello,',firstname, lastname,'! Welcome to CTI-100')
